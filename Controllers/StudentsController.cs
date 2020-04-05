35ce374389143f702aacb563b8a885adc97347e4
Tomek <masterporki@gmail.com>
2020-04-05 23:42:48 +0200

    kolejna czesc zadan APBD

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cw3.DAL;
+using Cw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cw3.Controllers
@@ -10,6 +12,16 @@ namespace Cw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
+        private readonly IDbService _dbService;
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
         [HttpGet("{id}")]
         public IActionResult GetStudent(int id)
         {
@@ -22,6 +34,33 @@ namespace Cw3.Controllers
             }
             return NotFound("Nie znaleziono studenta");
             
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database
+            //... generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            if (id <= 1)
+            {
+                return BadRequest("Zle ID");
+            }
+            return Ok("Student zostal poprawnie usuniety");
+        }
+
+
+        [HttpPut]
+        public IActionResult PutStudent()
+        {
+
+            return Ok();
         }
     }
 }
